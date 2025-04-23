22ec8f1a964f139253aa6da37c113f6514ef9d0a
Kristina Yaroshenko <kris.jaroshenko09@gmail.com>
2025-04-23 12:33:51 +0300

    Initial Commit

---
+++ JournalApp/JournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalAppApp.swift
+//  JournalApp
+//
+//  Created by Kristina Yaroshenko on 23.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct JournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JournalAppTests/JournalAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  JournalAppTests.swift
+//  JournalAppTests
+//
+//  Created by Kristina Yaroshenko on 23.04.2025.
+//
+
+import Testing
+@testable import JournalApp
+
+struct JournalAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
